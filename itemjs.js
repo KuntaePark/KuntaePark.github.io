9c806feb84282b75669f05519491e80c4f722b5e
Shynar Torekhan <shynartorekhan@gmail.com>
2018-05-13 23:54:40 +0900

    srch bar is fully functional
    srch bar is fully functional

--- itemjs.js
+++ itemjs.js
@@ -6,19 +6,64 @@ firebase.initializeApp(config);
 var database = firebase.database();
 var itemsRef = database.ref('items');
 var selectionsRef = database.ref('selections');
+var srchsRef = database.ref('srchs');
 var table = document.getElementById("table")
 var name = ""
+var lst = []
+
+var commentsRef = database.ref('comments');
+var itemCommentsRef = null;
+var itemKey;
+
+var curUserName = "John"
+var curUserCntry = "Indonesia"
+var curDate //temporary measure
+
+
 
 $( document ).ready(function() {
 	getname()
 	fillpage()
+  generatelst()
+  $('#srch').autocomplete({
+    minCharacters : 2,
+    source: lst,
+    select: function (e, ui) {
+      var obj = {
+        ans : ui.item.label
+      }
+      selectionsRef.push(obj)
+      location.href = "item.html";
+      return false
+  }
+  })
+  
 })
+
+document.getElementById("srch").addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+      var obj = {
+        ans : document.getElementById("srch").value
+      }
+      srchsRef.push(obj)
+      location.href = "webs.html";
+  }})
+
 
 function getname() {
 	selectionsRef.once("value", function(selections) {
 	            selections.forEach(function(selection){
 	              name = selection.val().ans
                 selectionsRef.remove()
+                itemsRef.orderByChild("engname").equalTo(name).once('value',function(snapshot) {
+                  itemKey = Object.keys(snapshot.val())[0]
+                  itemCommentsRef = commentsRef.child(itemKey)
+                  itemCommentsRef.on('value', function (snapshot) {
+                    var commentsObject = snapshot.val()
+                    renderComments(commentsObject)
+                  })
+               })
 	            });
 	          });
 }
@@ -64,3 +109,85 @@ function fillpage() {
             })
           });
 }
+
+function generatelst() {
+  itemsRef.once("value", function(items) {
+            items.forEach(function(item){
+              lst.push(item.val().engname)
+            });
+          });
+}
+
+// Firebase Database setup
+// Initialize Firebase
+
+// Bind comments to firebase
+
+
+function addMessage(usrname, cntry, date, message) {
+  var messageObject = {
+    usrname: usrname,
+    cntry: cntry,
+    date: date,
+    message: message,
+  }
+  itemCommentsRef.push(messageObject)
+}
+
+function renderComments(comments) {
+  var htmls;
+  if(comments == null) {
+    htmls = null;
+  } else {
+    htmls = Object.values(comments).map(function (comment) {
+      return `
+      <div class="comment">
+        <a class="avatar">
+          <img src="https://semantic-ui.com/images/avatar/small/joe.jpg">
+        </a>
+        <div class="content">
+          <a class="author">${comment.usrname}</a>
+          <div class="metadata">
+            <div>${comment.cntry}</div>
+            <div class="date">${comment.date}</div>
+          </div>
+          <div class="text">
+            ${comment.message}
+          </div>
+        </div>
+      </div>
+
+    `
+    })
+  }
+  $('#commentWrap').html(htmls)
+}
+
+$('#commentbtn').on("click",function (e) {
+  e.preventDefault()
+  var message = $('.field textarea').val()
+
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0!
+  var yyyy = today.getFullYear();
+
+  if(dd<10) {
+      dd = '0'+dd
+  } 
+
+  if(mm<10) {
+      mm = '0'+mm
+  } 
+
+  today = mm + '/' + dd + '/' + yyyy;
+  curDate = today
+  addMessage(curUserName,curUserCntry,curDate,message)
+  return false
+})
+
+$('#message').on("click",function() {
+  if(curUserName == null) {
+    //connect to log in functionality
+  }
+}); 
