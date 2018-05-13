36de88806a3537a9f053650c37f3d132720f5f30
Shynar Torekhan <shynartorekhan@gmail.com>
2018-05-14 01:59:38 +0900

    added report

--- itemjs.js
+++ itemjs.js
@@ -191,3 +191,54 @@ $('#message').on("click",function() {
     //connect to log in functionality
   }
 }); 
+
+
+document.getElementById("btn").onclick = function(){
+  $('.ui.report.modal')
+  .modal('show')
+;
+}
+
+$('.ui.report.form')
+  .form({
+    fields: {
+      reportinput: {
+        identifier: 'reportinput',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please insert the wrong information'
+        
+          }
+        ]
+      },
+    }
+  });
+
+$('.ui.red.report.cancel.basic.button').click(function(){
+console.log('click');
+$('.ui.report.modal.form').modal('hide'); 
+}),
+
+  $('.ui.green.refe.approve.basic.button').click(function(){
+console.log('click');
+$('.ui.refe.modal').modal('hide'); 
+}),
+ 
+ $('.ui.green.report.submit.basic.button').click(function(){
+ console.log('click');
+ if( $('.ui.report.form').form('is valid')) {
+ console.log('valid');
+ $('.ui.report.modal').modal('hide'); 
+ 
+  }
+     });
+
+$('.ui.green.report.submit.basic.button').click(function(){
+ console.log('click');
+ if( $('.ui.report.form').form('is valid')) {
+ console.log('valid');
+ $('.ui.refe.modal').modal('show'); 
+  
+  }
+     });
